5eba8236e2fcce297e9eed56e89481e158061656
Kareem Amin <k@kareemamin.com>
2020-06-26 17:39:54 -0400

    refactor: made functions annonymous, removed all the notes, linked to readme for more information, simplfiied the sample code

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+    This is an Action Package. It consists of one more Actions. 
+    Add an Action by importing it's action-definition.js and adding it to the actionDefinitions array
+*/
+
+const getTopRedditPosts = require("./actions/get-reddit-posts/action-definition.js")
+
+module.exports =  {
+  name: "Reddit-Action-Package", 
+  description: "Actions to search and get posts from Reddit", 
+  actionDefinitions: [getTopRedditPosts], 
+}
